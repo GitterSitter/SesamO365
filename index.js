47a63a63a7b44e905b5a497987c95b997665105d
Trond <trondtufte89@gmail.com>
2017-10-03 12:36:12 +0200

    changed array

--- index.js
+++ index.js
@@ -270,8 +270,8 @@ data.forEach(function (element) {
 
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
+    // console.log('content-type:', res.headers['content-type']);
+    // console.log('content-length:', res.headers['content-length']);
 
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
