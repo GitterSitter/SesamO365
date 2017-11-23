1e6f9d35900c602aa00516624a3af5cca167788f
trondkjetil <trondtufte89@gmail.com>
2017-11-23 19:53:22 +0100

    fix

--- index.js
+++ index.js
@@ -95,18 +95,19 @@ async function updateIndustryList(response, request) {
         var userArray = JSON.parse(body);
         newInstances = userArray;
 
-      for(var i = 0; i < existingInstances.length; i++ ){
+      for(var x = 0; x < userArray.length; x++ ){
        //   existingInstances.forEach(instance => {
-              console.log(i);
-              userArray.forEach(item => {
-
-              if (existingInstances[i]["fields"]["Title"] === item["values"]["no"]) {
-                newInstances.splice(item, 1);
-                console.log("Skipping " +  item["values"]["no"]);
+              console.log(x);
+          //    userArray.forEach(item => {
+            for(var i = 0; i < existingInstances.length; i++ ){
+              if (existingInstances[i]["fields"]["Title"] === userArray["values"]["no"]) {
+                newInstances.splice(userArray[x], 1);
+                console.log("Skipping " +  userArray[x]["values"]["no"]);
               }
-              console.log(existingInstances[i]["fields"]["Title"] + " === " +  item["values"]["no"]);
-            });
+              console.log(existingInstances[i]["fields"]["Title"] + " === " +  userArray[x]["values"]["no"]);
+           // });
           }
+        }
         // });
 
 
