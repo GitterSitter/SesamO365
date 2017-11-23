d5917aa11b9839f4cc9b0766131cd0ab32643a85
trondkjetil <trondtufte89@gmail.com>
2017-11-23 19:20:27 +0100

    fix

--- index.js
+++ index.js
@@ -109,17 +109,6 @@ async function updateIndustryList(response, request) {
         });
 
 
-
-    //   existingInstances.forEach(function(item1) {
-    //     item1.fields.Title = userArray.find(function (item2) {
-    //       if(item2["values"]["no"] === item1.fields.Title) {
-    //         newInstances.splice(item2, 1);
-    //         console.log("Removing: " + item2);
-    //       }
-    //     });
-    // });
-    
-     
         console.log(newInstances.length + " new items to insert");
 
       if(existingInstances === 0){
