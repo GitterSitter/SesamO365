9b0cd700d4fa3d2f17d9937cb20435d24cf05535
trondkjetil <trondtufte89@gmail.com>
2017-11-24 09:53:11 +0100

    fix

--- index.js
+++ index.js
@@ -97,11 +97,12 @@ async function updateIndustryList(response, request) {
         newInstances = userArray;
 
         var count = 0;
-      for(var x = 0; x < userArray.length; x++ ){
-   
-            for(var i = 0; i < existingInstances.length; i++ ){
-              
-              if (existingInstances[i]["fields"]["Title"] === userArray[x]["values"]["no"]) {
+
+        for(var i = 0; i < existingInstances.length; i++ ){
+            for(var x = 0; x < userArray.length; x++ ){
+                
+
+              if (JSON.stringify(existingInstances[i]["fields"]["Title"]) === JSON.stringify(userArray[x]["values"]["no"])) {
                 console.log("Skipping ****************************" + userArray[x]["values"]["no"]);
                 newInstances.splice(userArray[x], 1);     
                 count++;
@@ -111,6 +112,8 @@ async function updateIndustryList(response, request) {
           }
         }
     
+        existingInstances.filter((n) => array2.includes(n))
+
         console.log(count + " equals found");
         console.log(newInstances.length + " new items to insert");
 
